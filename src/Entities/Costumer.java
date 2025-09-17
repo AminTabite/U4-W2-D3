661dbd83d801cbb006c7ae64fee3933f5d170783
AminTabite <Aminepicode13@gmail.com>
2025-09-17 15:23:49 +0200

    classi fatte

--- src/Entities/Costumer.java
+++ src/Entities/Costumer.java
@@ -1,13 +1,45 @@
 package Entities;
 
+import java.util.Random;
+import java.util.function.Supplier;
+
 public class Costumer {
 
+    Supplier<Long> RandomIdsup = () -> {
+        Random rndm = new Random();
+        return rndm.nextLong(1, 100000);
+
+    };
     //attributi
+    private Long id;
+    private String name;
+    private Integer tier;
 
 
     //costruttori
+    Costumer(String name, Integer tier) {
+        this.id = RandomIdsup.get();
+        this.name = name;
+        this.tier = tier;
 
+
+    }
 
     //metodi
 
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+
+
+    }
+
+    public Integer getTier() {
+        return tier;
+    }
 }
--- src/Entities/Order.java
+++ src/Entities/Order.java
@@ -1,4 +1,63 @@
 package Entities;
 
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Supplier;
+
 public class Order {
+
+    //attributi
+
+
+    Long id;
+    String status;
+    LocalDate orderdate;
+    LocalDate deliveryDate;
+    List<Product> products;
+    Costumer costumer;
+
+    //costruttori
+
+
+    Supplier<Long> RandomIdsup = () -> {
+        Random rndm = new Random();
+        return rndm.nextLong(1, 100000);
+
+    };
+
+    Order(LocalDate orderdate, LocalDate deliveryDate, List products, Costumer costumer) {
+
+        this.id = RandomIdsup.get();
+        this.orderdate = orderdate;
+        this.deliveryDate = deliveryDate;
+        this.products = products;
+        this.costumer = costumer;
+
+
+    }
+
+    //metodi
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public LocalDate getOrderdate() {
+        return orderdate;
+    }
+
+    public LocalDate getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
 }
